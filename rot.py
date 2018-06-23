398df9738fa2baad4d736c10617dee76a0864322
chanpaul1234572 <chanpaul1234572@gmail.com>
2018-06-24 02:38:21 +0800

    Update rot.py

--- rot.py
+++ rot.py
@@ -1,7 +1,26 @@
 #This py is using for decrypting simple rot encryption
 import sys
-if len(sys.argv) != 2:
-    print "usage: python rot.py filename"
+if len(sys.argv) > 4 or len(sys.argv) < 3:
+    print "usage: python rot.py filename [number]"
+elif len(sys.argv) == 3:
+    filename = sys.argv[1]
+    sys.stdout.write("Rot")
+    num = int(sys.argv[2])
+    print str(num) + ":"
+    for line in open(filename, 'r'):
+        for c in line:
+            if ord(c) >= 65 and ord(c) <= 90:
+                rc = ord(c) - 65
+                rc = (rc + num) % 26
+                rc = chr(rc + 65)
+                sys.stdout.write(rc)
+            elif ord(c) >= 97 and ord(c) <= 122:
+                rc = ord(c) - 97
+                rc = (rc + num) % 26
+                rc = chr(rc + 97)
+                sys.stdout.write(rc)
+            else:
+                sys.stdout.write(c)
 else:
     filename = sys.argv[1]
     sys.stdout.write("Rot")
